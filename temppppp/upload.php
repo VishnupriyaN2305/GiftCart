346c03e2a2e45425f9607c824471091fba275620
iamishwaryaa <83499083+iamishwaryaa@users.noreply.github.com>
2021-05-12 08:24:32 +0530

    Add files via upload

---
+++ temppppp/upload.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="images/g.webp" type="image/webp" sizes="16x16">    
+  <title>GIFTCART</title>
+</head>
+<body>
+added to cart
+<input type="button" name="continue shopping" value="continue shopping" onclick="document.location.href='product.php'"><br>
+</body>
+</html>
